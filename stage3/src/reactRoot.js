7aa4e5534405f2e2121409be8c026d635085a1fa
mukaiguan <kaimuguan@163.com>
2019-04-09 19:00:19 +0800

    feat(): stage3完成

--- stage3/src/reactRoot.js
+++ stage3/src/reactRoot.js
@@ -1,12 +1,64 @@
 import FiberNode from './fiberNode.js';
 import { FIBERTAGS } from './Constant.js';
 
+function createDomElement(elementType, elementProps) {
+    const domElement = document.createElement(elementType);
+    const elementConfig = elementProps.config;
+    const events = ['onClick', 'onChange'];
+    const customStyles = {
+        'fontSize': 'font-size',
+        'textAlign': 'text-align'
+    };
+
+    if (elementConfig) {
+        const configKeys = Object.keys(elementConfig);
+        configKeys.forEach(configKey => {
+            // 判断props里面是否有事件需要绑定
+            const configValue = elementConfig[configKey];
+            if (events.includes(configKey)) {
+                domElement[configKey.toLowerCase()] = configValue;
+            }
+
+            // 判断props里面是否有className需要处理
+            if (configKey === 'className') {
+                domElement.setAttribute('class', configValue);
+            }
+
+            // 判断props中是否有style需要处理
+            if (configKey === 'style') {
+                let styleString = '';
+                for (let styleItem in configValue) {
+                    if (configValue.hasOwnProperty(styleItem)) {
+                        if (customStyles.hasOwnProperty(styleItem)) {
+                            const validStyleName = customStyles[styleItem];
+                            customStyles[validStyleName] = configValue[styleItem];
+                            styleString += `${validStyleName}: ${configValue[styleItem]};`;
+                            delete configValue[styleItem];
+                            continue;
+                        }
+                        styleString += `${styleItem}: ${configValue[styleItem]};`;
+                    }
+                }
+                domElement.setAttribute('style', styleString);
+            }
+        });
+    }
+
+    // 处理字符串类型的children
+    const children = elementProps.children;
+    if (typeof children === 'string') {
+        domElement.textContent = children;
+    }
+    return domElement;
+}
+
 class ReactRoot {
     constructor(container) {
         this.root = new FiberNode(FIBERTAGS.HostRoot, null, null, null);
         this.root.stateNode = container;
     }
 
+    // 生成Fiber tree
     render(reactElement) {
         function getChildFiber(currentElement, parentFiberNode) {
             // 深度优先遍历，生成Fiber tree
@@ -24,9 +76,12 @@ class ReactRoot {
                 children = instance.render();
             } else if (typeof currentElementType === 'string') {
                 newFiberNode = new FiberNode(FIBERTAGS.HostComponent, currentElementProps, currentElementKey, currentElementType);
+                // 对于元素类型的，这里先生成要装入到页面的元素
+                newFiberNode.stateNode = createDomElement(currentElementType, currentElementProps);
             }
             // 设置当前节点的父节点
             newFiberNode.return = parentFiberNode;
+            // 判断children是否需要递归
             if (children) {
                 // reactElment的children分为数组、reactElement对象、字符串
                 const childrenType = typeof children;
